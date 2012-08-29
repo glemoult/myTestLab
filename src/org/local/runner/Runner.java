2dbdebb7c07041ff0d5dc91707a366e76ccd2736
GLM <glm@glm.com>
2012-08-30 00:56:13 +0200

    Adding files

---
+++ src/org/local/runner/Runner.java
@@ -0,0 +1,16 @@
+package org.local.runner;
+
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aa");
+		double myDouble = 3.6;
+		int myInt = (int)myDouble;
+		System.out.println(myInt);
+	}
+
+}
